08c5e33cc13f91d9d211f04d4375904ad968b880
Ritesh-17 <79408851+Ritesh-17@users.noreply.github.com>
2021-06-07 04:16:31 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,38 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.image = loadImage("block.png");
+
+      this.Visiblity = 225;
+    }
+    
+    display(){
+
+      if(this.body.speed < 3){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+     else{
+      World.remove(world, this.body);
+      push();
+      this.Visiblity = this.Visiblity - 5;
+      tint(255,this.Visiblity);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+      pop();
+    }
+  }
+}
